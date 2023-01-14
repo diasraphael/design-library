6aaece9271e6a87fee0d2a61f35790a39b355a07
diasraphael <diasraphael88@gmail.com>
2023-01-14 18:39:39 +0100

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to design system</div>;
+  return <div className="App text-red-900">Welcome to design system</div>;
 }
 
 export default App;
